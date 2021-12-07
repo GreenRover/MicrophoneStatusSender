d7ab1168fcd87120ecb64bd8d3b78ce6b25b7df7
ue85540 <heiko.henning@sbb.ch>
2021-12-07 07:55:36 +0100

    Remove proxy support, because it seams not to work well

--- HttpUtils.cpp
+++ HttpUtils.cpp
@@ -9,12 +9,7 @@ void HttpUtils::callGet(LPWSTR url) {
 	WCHAR szHost[256];
 	DWORD dwOpenRequestFlag = 0;
 	URL_COMPONENTS urlComp;
-	BOOL fRet = FALSE;
 	HINTERNET hSession;
-
-	WINHTTP_AUTOPROXY_OPTIONS AutoProxyOptions = { 0 };
-	WINHTTP_CURRENT_USER_IE_PROXY_CONFIG IEProxyConfig;
-	WINHTTP_PROXY_INFO  proxyInfo = { 0 };
 
 	// Initialize URL_COMPONENTS structure.
 	ZeroMemory(&urlComp, sizeof(urlComp));
@@ -32,15 +27,12 @@ void HttpUtils::callGet(LPWSTR url) {
 	// Crack HTTP scheme.
 	urlComp.dwSchemeLength = -1;
 
-	DWORD dwAccessType = WINHTTP_ACCESS_TYPE_AUTOMATIC_PROXY; // WINHTTP_ACCESS_TYPE_DEFAULT_PROXY
-	bool bAutomaticProxyConfiguration = false;
-
 	// Create the session handle using the default settings.
-	hSession = WinHttpOpen(L"Asynchronous WinHTTP Demo/1.1",
-		WINHTTP_ACCESS_TYPE_DEFAULT_PROXY,
+	hSession = WinHttpOpen(L"MicrophoneStatusSender 1.1",
+		WINHTTP_ACCESS_TYPE_NO_PROXY,
 		WINHTTP_NO_PROXY_NAME,
 		WINHTTP_NO_PROXY_BYPASS,
-		WINHTTP_FLAG_ASYNC);
+		0);
 
 	// Check to see if the session handle was successfully created.
 	if (hSession == NULL)
@@ -55,113 +47,28 @@ void HttpUtils::callGet(LPWSTR url) {
 	}
 
 	// Open an HTTP session.
-	auto hConnect = WinHttpConnect(hSession, szHost, urlComp.nPort, 0);
+	HINTERNET hConnect = WinHttpConnect(hSession, szHost, urlComp.nPort, 0);
 	if (hConnect == NULL)
 	{
 		throw std::invalid_argument("WinHttpConnect failed: " + GetLastError());
 	}
 
-	if (bAutomaticProxyConfiguration)
-	{
-		if (WinHttpGetIEProxyConfigForCurrentUser(&IEProxyConfig))
-		{
-			//
-			// If IE is configured to autodetect, then we'll autodetect too
-			//
-			if (IEProxyConfig.fAutoDetect)
-			{
-				AutoProxyOptions.dwFlags = WINHTTP_AUTOPROXY_AUTO_DETECT;
-
-				//
-				// Use both DHCP and DNS-based autodetection
-				//
-				AutoProxyOptions.dwAutoDetectFlags = WINHTTP_AUTO_DETECT_TYPE_DHCP |
-					WINHTTP_AUTO_DETECT_TYPE_DNS_A;
-			}
-
-			//
-			// If there's an autoconfig URL stored in the IE proxy settings, save it
-			//
-			if (IEProxyConfig.lpszAutoConfigUrl)
-			{
-				AutoProxyOptions.dwFlags |= WINHTTP_AUTOPROXY_CONFIG_URL;
-				AutoProxyOptions.lpszAutoConfigUrl = IEProxyConfig.lpszAutoConfigUrl;
-			}
-
-			//
-			// If there's a static proxy
-			//
-			if (IEProxyConfig.lpszProxy)
-			{
-				AutoProxyOptions.dwFlags |= WINHTTP_AUTOPROXY_ALLOW_STATIC;
-			}
-
-			BOOL bResult = WinHttpGetProxyForUrl(hSession,
-				urlComp.lpszScheme,
-				&AutoProxyOptions,
-				&proxyInfo);
-
-			if (!bResult)
-			{
-				throw std::invalid_argument("WinHttpGetProxyForUrl failed: " + GetLastError());
-			}
-			else
-			{
-				/*
-				// WinHttpOpen dwAccessType values (also for WINHTTP_PROXY_INFO::dwAccessType)
-				#define WINHTTP_ACCESS_TYPE_DEFAULT_PROXY               0
-				#define WINHTTP_ACCESS_TYPE_NO_PROXY                    1
-				#define WINHTTP_ACCESS_TYPE_NAMED_PROXY					3
-				*/
-
-				if (!WinHttpSetOption(hSession,
-					WINHTTP_OPTION_PROXY,
-					&proxyInfo,
-					sizeof(proxyInfo)))
-				{
-					throw std::invalid_argument("WinHttpSetOption WINHTTP_OPTION_PROXY failed: " + GetLastError());
-				}
-			}
-		}
-	}
 	// Prepare OpenRequest flag
-	dwOpenRequestFlag = (INTERNET_SCHEME_HTTPS == urlComp.nScheme) ?
-		WINHTTP_FLAG_SECURE : 0;
+	dwOpenRequestFlag = (INTERNET_SCHEME_HTTPS == urlComp.nScheme) ? WINHTTP_FLAG_SECURE : 0;
 
 	// Open a "GET" request.
 	auto hRequest = WinHttpOpenRequest(hConnect,
-		L"GET", urlComp.lpszUrlPath,
-		NULL, WINHTTP_NO_REFERER,
+		L"GET",
+		urlComp.lpszUrlPath,
+		NULL,
+		WINHTTP_NO_REFERER,
 		WINHTTP_DEFAULT_ACCEPT_TYPES,
-		dwOpenRequestFlag);
+		dwOpenRequestFlag
+	);
 
 	if (hRequest == 0)
 	{
 		throw std::invalid_argument("WinHttpOpenRequest failed: " + GetLastError());
-	}
-
-	WINHTTP_PROXY_INFO         ProxyInfo;
-	DWORD                      cbProxyInfoSize = sizeof(ProxyInfo);
-
-
-	if (!WinHttpQueryOption(
-		hRequest,
-		WINHTTP_OPTION_PROXY,
-		&ProxyInfo,
-		&cbProxyInfoSize))
-	{
-		// Exit if setting the proxy info failed.	
-		throw std::invalid_argument("WinHttpQueryOption WINHTTP_OPTION_PROXY failed: " + GetLastError());
-	}
-	else
-	{
-		/*
-		// WinHttpOpen dwAccessType values (also for WINHTTP_PROXY_INFO::dwAccessType)
-		#define WINHTTP_ACCESS_TYPE_DEFAULT_PROXY               0
-		#define WINHTTP_ACCESS_TYPE_NO_PROXY                    1
-		#define WINHTTP_ACCESS_TYPE_NAMED_PROXY					3
-		*/
-		// printf("\tAccessType : %d\r\n", ProxyInfo.dwAccessType);
 	}
 
 	// Send the request.
@@ -171,5 +78,14 @@ void HttpUtils::callGet(LPWSTR url) {
 	{
 		throw std::invalid_argument("WinHttpSendRequest failed: " + GetLastError());
 	}
-	fRet = TRUE;
+
+
+	if (!WinHttpReceiveResponse(hRequest, NULL)) {
+		throw std::invalid_argument("WinHttpReceiveResponse failed: " + GetLastError());
+	}
+
+	DWORD SizeHeaders = 0;
+	LPCWSTR header;
+	WinHttpQueryHeaders(hRequest, 22, NULL, (LPVOID)&header, &SizeHeaders, NULL);
+
 }
